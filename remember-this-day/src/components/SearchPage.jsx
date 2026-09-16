4fb4c75b0c9e7ce251687be128c3ae1637bdabf4
TMcMurtry <mr.timmcmurtry@gmail.com>
2026-09-15 20:10:37 -0500

    Finished try/catch code, starting on useEffect logic

--- remember-this-day/src/components/SearchPage.jsx
+++ remember-this-day/src/components/SearchPage.jsx
@@ -1,18 +1,23 @@
 import './EntrySubmission.css';
-import { useState } from 'react';
+import { useState useEffect } from 'react';
 import './SearchPage.css';
 
 export default function EntrySubmission({currentUser}){
+    const [entries, setEntries] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
 
     async function findEntries() {
         try {
         const response = await fetch("http:localhost://8080/entries/user" + currentUser.id);
         const data = await response.json();
+        setEntries(data);
         if (!response.ok){
             throw new Error(`HTTP error: ${response.status} - could not connect to the database`)
             }
+        
         } catch (error){
-            
+            setErrorMessage(error.message);
         }
     }
 
