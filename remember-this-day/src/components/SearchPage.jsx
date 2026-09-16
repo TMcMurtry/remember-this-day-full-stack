ae38c851cd5abb8e680c9f1a7077810e851d220c
TMcMurtry <mr.timmcmurtry@gmail.com>
2026-09-15 19:57:43 -0500

    Started try/catch code for retrieving entries

--- remember-this-day/src/components/SearchPage.jsx
+++ remember-this-day/src/components/SearchPage.jsx
@@ -3,7 +3,18 @@ import { useState } from 'react';
 import './SearchPage.css';
 
 export default function EntrySubmission({currentUser}){
-    
+
+    async function findEntries() {
+        try {
+        const response = await fetch("http:localhost://8080/entries/user" + currentUser.id);
+        const data = await response.json();
+        if (!response.ok){
+            throw new Error(`HTTP error: ${response.status} - could not connect to the database`)
+            }
+        } catch (error){
+            
+        }
+    }
 
 
     return(
