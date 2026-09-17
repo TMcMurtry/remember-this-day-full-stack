c6c834a898018debf48f7116e268d5704661c167
TMcMurtry <mr.timmcmurtry@gmail.com>
2026-09-17 14:39:23 -0500

    tentatively finished logic for search page

--- remember-this-day/src/components/SearchPage.jsx
+++ remember-this-day/src/components/SearchPage.jsx
@@ -9,7 +9,15 @@ export default function EntrySubmission({currentUser}){
     const [searchInput, setSearchInput] = useState("");
     const [matchingEntries, setMatchingEntries] = useState("");
     const [chosenEntry, setChosenEntry] = useState("");
+    const [dateField, setDateField] = useState("");
+    const [titleField, setTitleField] = useState("");
+    const [entryField, setEntryField] = useState("");
+    const [editSuccess, setEditSuccess] = useState("");
+    const [deleteSuccess, setDeleteSuccess] = useState("");
     const handleSearchInput = (ev) => setSearchInput(ev.target.value);
+    const handleDateChange = (ev) => setDateField(ev.target.value);
+    const handleTitleChange = (ev) => setTitleField(ev.target.value);
+    const handleEntryChange = (ev) => setEntryField(ev.target.value);
     
     async function loadEntries() {
         try {
@@ -40,12 +48,11 @@ export default function EntrySubmission({currentUser}){
 
     async function editEntry(){
         try{
-            const response = await fetch("http://localhost:8080/entries", {method: "PUT",
+            const response = await fetch("http://localhost:8080/entries" + chosenEntry.id, {method: "PUT",
                 headers: {
                     "Content-type": "application/json"
                 },
                 body: JSON.stringify({
-                    id: chosenEntry.id,
                     title: chosenEntry.title,
                     entryText: chosenEntry.entryText,
                     date: chosenEntry.date,
@@ -57,12 +64,26 @@ export default function EntrySubmission({currentUser}){
                 throw new Error(`Could not connect to the database`)
             }
             setChosenEntry("");
+            setEditSuccess("Entry Successfully Updated")
         } catch (error) {
             setSearchError(true);
             setErrorMessage(error.message);
         }
     }
 
+    async function deleteEntry(){
+        try{
+            const response = await fetch("http://localhost:8080/entries" + chosenEntry.id, {method: "DELETE" }) 
+            if (!response.ok){
+                throw new Error(`Could not connect to the database`)
+            }
+            setChosenEntry("");
+            setDeleteSuccess("Entry Successfully Deleted")
+        } catch (error) {
+            setSearchError(true);
+            setErrorMessage(error.message);
+        }
+    }
     
 
 
@@ -72,18 +93,28 @@ export default function EntrySubmission({currentUser}){
             <label htmlFor='searchField'>
                 <input name="searchField" id="searchField" type="text" value={searchInput} onChange={handleSearchInput} placeholder="Enter Title"/>
             </label>
+            {searchError && <p>{errorMessage}</p>}
             {noMatch && <p>{noMatch}</p>}
             <li>
-            {matchingEntries.map((entry) => <ul onClick={selectEntry}>Date: {entry.date} <br/> Title: {entry.title} <br/> Entry: {entry.entryText.slice(0,50)} </ul> )}
+            {matchingEntries.map((entry) => <ul onClick={setChosenEntry(entry)}>Date: {entry.date} <br/> Title: {entry.title} <br/> Entry: {entry.entryText.slice(0,50)} </ul> )}
             </li>
-            <div className='chosenSearchResult'>
-                <h4>Edit or Remove Entry</h4>
-                <P>Date: {chosenEntry.date}</P>
-                <p>Title: {chosenEntry.title}</p>
-                <p>Entry: {chosenEntry.entryText}</p>
-                <button name="edit" id="edit" type='button' onClick={editEntry}>Edit</button> 
-                <button name="delete" id="delete" type='button' onClick={deleteEntry}>Delete</button> 
-
-            </div>
+            {chosenEntry &&
+                <form className='chosenSearchResult' >
+                    <h4>Edit or Remove Entry</h4>
+                    <label htmlFor='title'>
+                        <input name="title" id="title" type="text" value={titleField} onChange={handleTitleChange} placeholder="Enter Title"/>
+                    </label>
+                    <label htmlFor='entry'>
+                        <input name="entry" id="entry" type="text" value={entryField} onChange={handleEntryChange} placeholder="Journal Entry"/>
+                    </label>
+                    <label htmlFor='date'>
+                        <input name="date" id="date" type='date' value={dateField} onChange={handleDateChange} placeholder="Date"/>
+                    </label>
+                    <button name="edit" id="edit" type='button' onClick={editEntry}>Edit</button> 
+                    <button name="delete" id="delete" type='button' onClick={deleteEntry}>Delete</button> 
+                    {editSuccess && <p>{editSuccess}</p>}
+                    {deleteSuccess && <p>{deleteSuccess}</p>}
+                </form>
+            }       
         </div>
     )};
