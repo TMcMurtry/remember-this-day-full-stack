4462334c7451d6725b8a515cb9fff133baa48627
TMcMurtry <mr.timmcmurtry@gmail.com>
2026-09-17 16:38:34 -0500

    Refactored CORS config file

--- remember-this-day-backend/src/main/java/com/launchcode/remember_this_day_backend/config/CorsConfig.java
+++ remember-this-day-backend/src/main/java/com/launchcode/remember_this_day_backend/config/CorsConfig.java
@@ -1,22 +1,21 @@
-package config;
+package com.launchcode.remember_this_day_backend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("http://localhost:5173");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedMethods("*");
     }
 }
