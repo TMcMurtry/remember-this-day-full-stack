9557019ace045d86b2b6740f17f2b7cfcbd36a79
TMcMurtry <mr.timmcmurtry@gmail.com>
2026-09-18 07:50:54 -0500

    Refactored CORS settings to avoid errors I was dealing with

--- remember-this-day-backend/src/main/java/com/launchcode/remember_this_day_backend/config/CorsConfig.java
+++ remember-this-day-backend/src/main/java/com/launchcode/remember_this_day_backend/config/CorsConfig.java
@@ -1,21 +1,25 @@
 package com.launchcode.remember_this_day_backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.filter.CorsFilter;
+import java.util.Collections;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowCredentials(true)
-                .allowedHeaders("*")
-                .allowedMethods("*");
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:5173"));
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
+
